c43f0b032ac1e9ca7a8917b832d48c68a8ab0487
haibara502 <haibara502@gmail.com>
2017-09-23 20:25:17 -0600

    margtin perceptron

--- Homework/HW2/train-develope-test/Margin Perceptron/margin_perceptron.cpp
+++ Homework/HW2/train-develope-test/Margin Perceptron/margin_perceptron.cpp
@@ -32,9 +32,9 @@ int main()
 	for (int epoch = 0; epoch < 20; ++epoch)
 	{
 		perceptron.train(urls);
-		cout << "This is epoch : " << epoch << endl;
+		cout << "This is epoch : " << epoch + 1<< endl;
 		cout << "The accuracy of development set is " << perceptron.cv(urls) << endl;
 		perceptron.divideLearningRate(epoch + 1);
 	}
-	cout << "The final accuracy of simple perceptron is " << perceptron.test(urls) << endl;
+	cout << "The final accuracy of margin perceptron is " << perceptron.test(urls) << endl;
 }
